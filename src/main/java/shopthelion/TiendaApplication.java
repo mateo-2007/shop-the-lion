ea05759bc13361df297d61c541a95970e771d6fb
mateo <brayanmateogarzonvargas@gmail.com>
2026-06-28 23:29:23 -0500

    Subida limpia del proyecto

--- src/main/java/shopthelion/TiendaApplication.java
+++ src/main/java/shopthelion/TiendaApplication.java
@@ -1,11 +1,13 @@
-package com.shopthelion;
+package com.shopthelion.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TiendaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TiendaApplication.class, args);
     }
+
 }
